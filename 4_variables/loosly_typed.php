abc42b9a3c3c7e315519d942fe959d9fd7bdebdb
nickgs <nickgs1@gmail.com>
2013-10-18 17:17:20 -0400

    Added/Removed some variable examples

--- 4_variables/loosly_typed.php
+++ 4_variables/loosly_typed.php
@@ -2,7 +2,7 @@
 
 /**
  * PHP is a loosley typed language.
- * This means we do not need to declare a variable type when declaring the variable.
+ * This means we do not need to declare a variable type when defining the variable.
  *
  */
 
@@ -13,4 +13,6 @@ $price = 15.95;
 echo "Name: $name \n";
 echo "Age: $age \n";
 echo "Price: $price \n";
+
+echo substr($price, 2);
 ?>
--- 4_variables/simple.php
+++ 4_variables/simple.php
@@ -3,10 +3,9 @@
  * A variable can be viewed as a bucket in our program that enables us to store information.
  * Syntax:
  *    Variables in PHP are defined by adding a dollar sign ($) followed by the name of the variable.
+ *    Variable names start with either a underscore OR letter, no numbers.
  **/
 
 //a simple variable to store a number:
 $my_number = 10;
-
-
 ?>
