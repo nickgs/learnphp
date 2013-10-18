0bf9ea9d156fad83acbca1a00b9bb95367c4d828
nickgs <nickgs1@gmail.com>
2013-10-18 17:21:42 -0400

    Added initial function examples

---
+++ 5_functions/functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Functions are defined in PHP using the function keyword.
+ * Like our variables they are not defined by type.
+ */
+function foo($arg_1, $arg_2, /* ..., */ $arg_n)
+{
+    echo "Example function.\n";
+    return $retval;
+}
+?>
