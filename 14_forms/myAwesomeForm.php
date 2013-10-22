f3f2a2ef89a5af92c5c6292e084a6915f76acd18
nickgs <nickgs1@gmail.com>
2013-10-21 21:28:24 -0400

    Added a basic form example

---
+++ 14_forms/myAwesomeForm.php
@@ -0,0 +1,18 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title></title>
+  </head>
+  <body>
+    <form id="my_form" method="POST" action="process.php">
+      <label for="name">Name:</label> 
+      <input name="name" type="textfield" size="60"/>
+      <input type="submit">
+    </form>
+  </body>
+</html>
---
+++ 14_forms/process.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * Simple PHP script to process POSTED form input.
+ */
+
+$name = $_POST['name'];
+
+echo "Hello $name";
+?>
